3b89362feef65734ebd428631ff64f463391717a
aseemchopra25 <aseemchopra@protonmail.com>
2021-10-06 23:52:57 +0530

    Updates

---
+++ precache-manifest.0c9f9a1a3b8e035dbb42ccc7232a6e1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0391275617578edc2e1742ce8d5bd6",
+    "url": "/dibi/index.html"
+  },
+  {
+    "revision": "ebf4c313f2904e20c825",
+    "url": "/dibi/static/css/main.31b6137c.chunk.css"
+  },
+  {
+    "revision": "4c71216431ab3c442027",
+    "url": "/dibi/static/js/2.95969962.chunk.js"
+  },
+  {
+    "revision": "cbe01fbbe035bf90d35a835621f136ef",
+    "url": "/dibi/static/js/2.95969962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf4c313f2904e20c825",
+    "url": "/dibi/static/js/main.c4986c00.chunk.js"
+  },
+  {
+    "revision": "f800c2426109f13a15e9",
+    "url": "/dibi/static/js/runtime-main.7f832aae.js"
+  },
+  {
+    "revision": "de28f71ec6eb8dfda2e68d2211ee49eb",
+    "url": "/dibi/static/media/AvenirNextLTPro-Demi.de28f71e.otf"
+  }
+]);
